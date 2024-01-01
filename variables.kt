a780ad0bc700837a6d246bebde472fee305d0079
Adel <adi.stein@outlook.cz>
2024-01-01 20:51:40 +0100

    Create variables.kt

---
+++ variables.kt
@@ -0,0 +1,10 @@
+
+val pi: Int = 3.14 //val definuje neměnou proměnou
+
+fun main() {
+    val name = "John"
+    // name = "Jane" // Chyba: Nelze přiřadit novou hodnotu k proměnné 'name'
+
+    var age = 25
+    age = 26 // OK, můžeme měnit hodnotu proměnné 'age'
+}
